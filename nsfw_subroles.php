cc75c4e58f8a4a3feffda0f5eed8bcbed8c0613f
Valithor Obsidion <valzargaming@gmail.com>
2021-01-12 12:45:49 -0500

    Placeholder roles

---
+++ nsfw_subroles.php
@@ -0,0 +1,11 @@
+<?php
+include_once 'custom_functions.php';
+
+$nsfwsubroles = array(
+    "Placeholder" => "🖌️"
+);
+
+//Message copy-pasta:
+$nsfwsubroles_message_text = "**Role Menu: NSFW Roles**
+:paintbrush: : `Art`
+";
