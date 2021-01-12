afe71cfab8f0837498143767946f17052c6b801e
Valithor Obsidion <valzargaming@gmail.com>
2021-01-12 18:47:47 -0500

    Webserver->Repositories

--- run.php
+++ run.php
@@ -94,6 +94,42 @@ $webapi = new Server($discord->getLoop(), function (ServerRequestInterface $requ
 				return webapiFail('guild_id', $id);
 			break;
 
+		case 'bans':
+			if (!$id || !webapiSnow($id) || !$guild = $discord->guilds->offsetGet($id))
+				return webapiFail('guild_id', $id);
+			$return = $guild->bans;
+			break;
+
+		case 'channels':
+			if (!$id || !webapiSnow($id) || !$guild = $discord->guilds->offsetGet($id))
+				return webapiFail('guild_id', $id);
+			$return = $guild->channels;
+			break;
+
+		case 'members':
+			if (!$id || !webapiSnow($id) || !$guild = $discord->guilds->offsetGet($id))
+				return webapiFail('guild_id', $id);
+			$return = $guild->members;
+			break;
+
+		case 'emojis':
+			if (!$id || !webapiSnow($id) || !$guild = $discord->guilds->offsetGet($id))
+				return webapiFail('guild_id', $id);
+			$return = $guild->emojis;
+			break;
+
+		case 'invites':
+			if (!$id || !webapiSnow($id) || !$guild = $discord->guilds->offsetGet($id))
+				return webapiFail('guild_id', $id);
+			$return = $guild->invites;
+			break;
+
+		case 'roles':
+			if (!$id || !webapiSnow($id) || !$guild = $discord->guilds->offsetGet($id))
+				return webapiFail('guild_id', $id);
+			$return = $guild->roles;
+			break;
+
 		case 'guildMember':
 			if (!$id || !webapiSnow($id) || !$guild = $discord->guilds->offsetGet($id))
 				return webapiFail('guild_id', $id);
@@ -121,7 +157,7 @@ $webapi = new Server($discord->getLoop(), function (ServerRequestInterface $requ
 			//execInBackground('cmd /c "'. __DIR__  . '\run.bat"');
 			//exec('/home/outsider/bin/stfc restart');
 			break;
-			
+
 		case 'lookup':
 			if (substr($request->getServerParams()['REMOTE_ADDR'], 0, 6) != '10.0.0'){ //This can be abused to cause 429's with Restcord and should only be used by the website. All other cases should use 'user'
 				echo '[REJECT]' . $request->getServerParams()['REMOTE_ADDR'] . PHP_EOL;
@@ -130,9 +166,26 @@ $webapi = new Server($discord->getLoop(), function (ServerRequestInterface $requ
 			if (!$id || !webapiSnow($id) || !$return = $discord->users->offsetGet($id))
 				return webapiFail('user_id', $id);
 			break;
-			
+
+		case 'owner':
+			if (substr($request->getServerParams()['REMOTE_ADDR'], 0, 6) != '10.0.0'){
+				echo '[REJECT]' . $request->getServerParams()['REMOTE_ADDR'] . PHP_EOL;
+				return new Response(501, ['Content-Type' => 'text/plain'], 'Reject'.PHP_EOL);
+			}
+			if (!$id || !webapiSnow($id))
+				return webapiFail('user_id', $id);
+			$return = false;
+			if ($user = $discord->users->offsetGet($id)){ //Search all guilds the bot is in and check if the user id exists as a guild owner
+				foreach ($discord->guilds as $guild){
+					if ($id == $guild->owner_id){
+						$return = true;
+						break 1;
+					}
+				}
+			}
+			break;
+
 		case 'whitelist':
-			return new Response(501, ['Content-Type' => 'text/plain'], 'Not implemented'.PHP_EOL); //Test pilot is being done with VZG Verifier
 			if (substr($request->getServerParams()['REMOTE_ADDR'], 0, 6) != '10.0.0'){
 				echo '[REJECT]' . $request->getServerParams()['REMOTE_ADDR'] . PHP_EOL;
 				return new Response(501, ['Content-Type' => 'text/plain'], 'Reject'.PHP_EOL);
@@ -163,7 +216,7 @@ $webapi = new Server($discord->getLoop(), function (ServerRequestInterface $requ
 				}
 			}
 			break;
-			
+
 		case 'avatar':
 			if (!$id || !webapiSnow($id)){
 				return webapiFail('user_id', $id);
@@ -183,7 +236,7 @@ $webapi = new Server($discord->getLoop(), function (ServerRequestInterface $requ
 			}
 			//if (!$return) return new Response(($id ? 404 : 400), ['Content-Type' => 'text/plain'], ('').PHP_EOL);
 			break;
-			
+
 		case 'avatars':
 			$idarray = $data ?? array(); // $data contains POST data
 			$results = [];
