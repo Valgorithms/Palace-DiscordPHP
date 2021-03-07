a4c5c188b2ddce02a17ca6658c6ba4afea7b6748
Valithor Obsidion <valzargaming@gmail.com>
2021-03-07 14:50:01 -0500

    Relay Twitch chat to Discord

--- run.php
+++ run.php
@@ -81,6 +81,54 @@ $rtmp->on('error',
 $rsocket = new \React\Socket\Server(sprintf('%s:%s', '0.0.0.0', '55554'), $loop);
 $rtmp->listen($rsocket);
 */
+require 'secret.php'; //$secret
+$options = array(
+	//Required
+	'secret' => $secret, // Client secret
+	'nick' => 'ValZarGaming', // Twitch username
+	'channel' => 'daathren', // Channel to join
+	
+	//Optional
+	'discord' => $discord, // Pass your own instance of DiscordPHP (https://github.com/discord-php/DiscordPHP)	
+	'discord_output' => true, // Output Twitch chat to a Discord server
+	'guild_id' => '116927365652807686', //ID of the Discord server
+	'channel_id' => '431415282129698866', //ID of the Discord channel to output messages to
+	
+	//'loop' => $loop, // Pass your own instance of $loop to share with other ReactPHP applications
+	'socket_options' => [
+        'dns' => '8.8.8.8', // Can change DNS provider
+	],
+	'verbose' => false, // Additional output to console (useful for debugging)
+	
+	//Custom commands
+	'commandsymbol' => [ // Process commands if a message starts with a prefix in this array
+		'!',
+		';',
+	],
+	'whitelist' => [ // Users who are allowed to use restricted functions
+		'daathren',
+	],
+	'responses' => [ // Whenever a message is sent matching a key and prefixed with a command symbol, reply with the defined value
+		'ping' => 'Pong!',
+		'github' => 'https://github.com/VZGCoders/TwitchPHP',
+		'discord' => 'https://discord.gg/yXJVTQNh9e',
+		
+	],
+	'functions' => [ // Enabled functions usable by anyone
+		'help', // Send a list of commands as a chat message
+	],
+	'restricted_functions' => [ // Enabled functions usable only by whitelisted users
+		'php', //Outputs the current version of PHP as a message
+	],
+	'private_functions' => [ // Enabled functions usable only by the bot owner sharing the same username as the bot
+		'stop', //Kills the bot
+	],
+);
+//include 'commands.php';
+//$options['commands'] => $commands; // Import your own Twitch/Commands object to add additional functions
+
+$twitch = new Twitch\Twitch($options);
+
 function webapiFail($part, $id){
 	//logInfo('[webapi] Failed', ['part' => $part, 'id' => $id]);
 	return new \GuzzleHttp\Psr7\Response(($id ? 404 : 400), ['Content-Type' => 'text/plain'], ($id ? 'Invalid' : 'Missing').' '.$part.PHP_EOL);
