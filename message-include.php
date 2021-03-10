82d34b8b0438c72b078f543f658eff1260e80040
Valithor Obsidion <valzargaming@gmail.com>
2021-03-09 22:21:47 -0500

    x2

--- message-include.php
+++ message-include.php
@@ -1863,7 +1863,7 @@ if(!$called) return;
 	if (str_starts_with($message_content_lower, 'leave #')){ //;leave #channel
 		$filter = 'leave #';
 		$value = explode(' ', str_replace($filter, "", $message_content_lower));
-		$twitch->joinChannel($value[0]);
+		$twitch->leaveChannel($value[0]);
 	}
 
     /*
