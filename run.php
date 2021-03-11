d2549c3e12280f59ca478f735439798d271dc637
Valithor Obsidion <valzargaming@gmail.com>
2021-03-10 21:06:05 -0500

    (Twitch) Sample commands

--- run.php
+++ run.php
@@ -118,12 +118,17 @@ $options = array(
 		'ping' => 'Pong!',
 		'github' => 'https://github.com/VZGCoders/TwitchPHP',
 		'discord' => 'https://discord.gg/yXJVTQNh9e',
+		'lurk' => 'You have said the magick word to make yourself invisible to all eyes upon you, allowing you to fade into the shadows.',
+		'social' => 'Come follow the magick through several dimensions:  Twitter - https://twitter.com/daathren |  Instagram - https://www.instagram.com/daathren/ |  Discord - https://discord.gg/FhzXf8VK |  Tumblr - https://daathren.tumblr.com/',
+		'tip' => 'Wanna help fund the magick?  PayPal - https://www.paypal.com/paypalme/daathren |  CashApp - $DAAthren',
+		'return' => 'You have rolled a Nat 1, clearing your invisibility buff from earlier. You might want to roll for initiative…',
 	],
 	'functions' => [ // Enabled functions usable by anyone
 		'help', // Send a list of commands as a chat message
 	],
 	'restricted_functions' => [ // Enabled functions usable only by whitelisted users
 		'stop', //Kills the bot
+		'so', //Advertise someone else
 	],
 	'private_functions' => [ // Enabled functions usable only by the bot owner sharing the same username as the bot
 		'php', //Outputs the current version of PHP as a message
