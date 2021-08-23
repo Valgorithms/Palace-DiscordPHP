709e73ef7cdccc739dd149fb3a52f286b7fa9c05
Valithor Obsidion <valzargaming@gmail.com>
2021-08-23 00:24:35 -0400

    Update messagereactionadd-function.php

--- functions/messagereactionadd-function.php
+++ functions/messagereactionadd-function.php
@@ -63,7 +63,7 @@ function messageReactionAdd($reaction, $discord) {
 	//Role picker stuff
 	$message_id	= $message->id;														//if($GLOBALS['debug_echo']) echo "message_id: " . $message_id . PHP_EOL;
 	global $gameroles, $species, $species2, $species3, $sexualities, $gender, $pronouns, $channelroles, $nsfwroles, $nsfwsubroles;
-	$guild_custom_roles_path = getcwd()  . "\\$guild_folder\\custom_roles.php";
+	$guild_custom_roles_path = getcwd()  . "\$guild_folder\\custom_roles.php";
 	echo 'guild_custom_roles_path: ' . $guild_custom_roles_path;
 	if (CheckFile($guild_folder."\\", 'custom_roles.php')) {
 		include "$guild_custom_roles_path"; //Overwrite default custom_roles
